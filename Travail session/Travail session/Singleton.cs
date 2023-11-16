f3daeb6a1fb1379792a521fc89df5dbcdcedf78e
Maath3211 <mathys.l.lessard@gmail.com>
2023-11-16 14:28:55 -0500

    ajout client

--- Travail session/Travail session/Singleton.cs
+++ Travail session/Travail session/Singleton.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.WindowsAppSDK.Runtime.Packages;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,28 @@ namespace Travail_session
 {
     internal class Singleton
     {
+
+        ObservableCollection<clients> listeClients;
+        static Singleton instance = null;
+        public Singleton()
+        {
+            listeClients = new ObservableCollection<clients>();
+            listeClients.Add(new clients(100, "Mathys Lessard", "2371 lac-rond", "mathys.l.lessard@gmail.com", "819-222-2222"));
+        }
+
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+
+            return instance;
+        }
+
+        public ObservableCollection<clients> getListeClients()
+        {
+            return listeClients;
+        }
+
     }
 }
---
+++ Travail session/Travail session/clients.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travail_session
+{
+    internal class clients
+    {
+        int id;
+        string nom, adresse, email, telephone;
+
+        public clients(int id, string nom, string adresse, string email, string telephone)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.adresse = adresse;
+            this.email = email;
+            this.telephone = telephone;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public string Email { get => email; set => email = value; }
+        public string Telephone { get => telephone; set => telephone = value; }
+
+        
+
+        //listeClients.Add(new clients(100, "Mathys Lessard", "2371 lac-rond", "mathys.l.lessard@gmail.com","819-222-2222"));
+
+
+
+
+
+
+    }
+}
