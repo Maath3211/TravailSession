d8b974a794590643c7053dddab64903066096498
naijel2103 <loick.michaud2103@gmail.com>
2023-11-16 14:15:39 -0500

    Envoie classe Employes/Projets

---
+++ Travail session/Travail session/employes.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travail_session
+{
+    internal class employes
+    {
+        string matricule;
+        string nom;
+        string prenom;
+        string dateNaissance;
+        string naissance;
+        string adresse;
+        string dateEmbauche;
+        double tauxHoraire;
+        string photo;
+        string statut;
+
+        public employes(string matricule, string nom, string prenom, string dateNaissance, string naissance, string adresse, string dateEmbauche, double tauxHoraire, string photo, string statut)
+        {
+            this.matricule = matricule;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.dateNaissance = dateNaissance;
+            this.naissance = naissance;
+            this.adresse = adresse;
+            this.dateEmbauche = dateEmbauche;
+            this.tauxHoraire = tauxHoraire;
+            this.photo = photo;
+            this.statut = statut;
+        }
+
+        public string Matricule { get => matricule; set => matricule = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string DateNaissance { get => dateNaissance; set => dateNaissance = value; }
+        public string Naissance { get => naissance; set => naissance = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public string DateEmbauche { get => dateEmbauche; set => dateEmbauche = value; }
+        public double TauxHoraire { get => tauxHoraire; set => tauxHoraire = value; }
+        public string Photo { get => photo; set => photo = value; }
+        public string Statut { get => statut; set => statut = value; }
+    }
+}
---
+++ Travail session/Travail session/projets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travail_session
+{
+    internal class projets
+    {
+        string numeroProjet;
+        string titre;
+        string dateDebut;
+        string description;
+        double budget;
+        int nbrEmploye;
+        double totalSalaire;
+        int idClient;
+        string statut;
+
+        public projets(string numeroProjet, string titre, string dateDebut, string description, double budget, int nbrEmploye, double totalSalaire, int idClient, string statut)
+        {
+            this.numeroProjet = numeroProjet;
+            this.titre = titre;
+            this.dateDebut = dateDebut;
+            this.description = description;
+            this.budget = budget;
+            this.nbrEmploye = nbrEmploye;
+            this.totalSalaire = totalSalaire;
+            this.idClient = idClient;
+            this.statut = statut;
+        }
+
+        public string NumeroProjet { get => numeroProjet; set => numeroProjet = value; }
+        public string Titre { get => titre; set => titre = value; }
+        public string DateDebut { get => dateDebut; set => dateDebut = value; }
+        public string Description { get => description; set => description = value; }
+        public double Budget { get => budget; set => budget = value; }
+        public int NbrEmploye { get => nbrEmploye; set => nbrEmploye = value; }
+        public double TotalSalaire { get => totalSalaire; set => totalSalaire = value; }
+        public int IdClient { get => idClient; set => idClient = value; }
+        public string Statut { get => statut; set => statut = value; }
+    }
+}
